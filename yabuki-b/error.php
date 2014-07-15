8c1534e9274f4acf5633fec28efb7ca261859728
takumimiyake <s1242109FS@s.chibakoudai.jp>
2014-07-15 17:29:59 +0900

    Create error.php

---
+++ yabuki-b/error.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width,initial-scale=1.0">
+        <title>ログインエラー</title>
+    </head>
+    <body>
+    <center>
+        <h2>滞在管理システム</h2>
+        <br>
+        <br>
+        <br>
+        <font size="6" color="red">ログインに失敗しました</font>
+        <br>
+        <br>
+        <br>
+        <br>
+        <form action="logingamen.php" method="post">
+            <input type="submit" value="戻る" />
+        </form>
+    </center>
+</body>
+</html>
