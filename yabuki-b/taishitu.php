51739386002162502ca2710c56e11617c3b9157c
takumimiyake <s1242109FS@s.chibakoudai.jp>
2014-07-15 17:49:02 +0900

    Create taishitu.php

---
+++ yabuki-b/taishitu.php
@@ -0,0 +1,28 @@
+<html>
+    <head>
+        <meta charset='utf-8' />
+    </head>
+    <body>
+        <?php
+        session_start();
+        $pass = $_SESSION['pass'];
+
+//データベースに接続
+
+    error_reporting(E_ALL & ~E_NOTICE);
+    //データベースに接続
+require_once 'database_conf.php';
+
+
+//「滞在」に変更（レシピ267）
+        
+        $sql = "UPDATE users SET inroom = 0 WHERE pass = :pass";
+        $prepare = $db->prepare($sql);
+        $prepare->bindValue(':pass', $pass);
+
+        $prepare->execute();
+
+        echo "変更しました。<a href='main.php'>メインメニューへ</a>";
+        ?>        
+    </body>
+</html>
