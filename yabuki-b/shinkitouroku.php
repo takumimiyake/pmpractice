e40c14df1f3d240e814f8527ca8fa6faac2453a4
takumimiyake <s1242109FS@s.chibakoudai.jp>
2014-07-17 22:50:34 +0900

    Update shinkitouroku.php

--- yabuki-b/shinkitouroku.php
+++ yabuki-b/shinkitouroku.php
@@ -6,64 +6,68 @@
     </head>
     <body>
     <center>
-   <h1>
-           初回登録画面   
+        <h1>
+            初回登録画面   
         </h1>
         <br>
-        
-<p>名前と、パスワードを8桁で入力し、  <br>
-所属している研究室を選択して下さい。
-</p>
 
+        <p>名前とパスワードを入力し、  <br>
+            所属している研究室を選択して下さい。
+        </p>
+
+   
         <div>
-       
+
             <form method="post" action="adduser.php">
                 <CENTER>
-                    <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;名前<input type="text"size="30" name="example1" value="" ><br>
-                    
+                    <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;名前<input type="text"size="30" name="example1" value=""required ><br>
+
                     <CENTER>
-         &nbsp;&nbsp;&nbsp;パスワード<input type="password" name="example3"size="30" value="" pattern="[A-Za-z0-9]{8,8}" required ><br>
-                    <br>
-                    <CENTER>
-                      &nbsp;所属研究室
+                        &nbsp;&nbsp;&nbsp;パスワード<input type="password" name="example3"size="30" value="" pattern="[A-Za-z0-9]{1,36}"required  ><br>
+                        <br>
+                        <CENTER>
+                            &nbsp;所属研究室
 
-<select style="width: 215px" name="example2" ><br />>
+                            <select style="width: 215px" name="example2"required ><br />>
 
- <option value=""selected></option>
+                                <option value=""selected></option>
 
-<option value="1">堀内研究室</option><br>
+                                <option value="1">堀内研究室</option><br>
 
-<option value="2">竹本研究室</option><br>
+                                <option value="2">竹本研究室</option><br>
 
-<option value="3">矢吹研究室</option><br>
+                                <option value="3">矢吹研究室</option><br>
 
-<option value="4">下田研究室</option><br>
+                                <option value="4">下田研究室</option><br>
 
-<option value="5">田隈研究室</option><br>
+                                <option value="5">田隈研究室</option><br>
 
-<option value="6">五百井研究室</option><br>
+                                <option value="6">五百井研究室</option><br>
 
-<option value="7">久保研究室</option><br>
+                                <option value="7">久保研究室</option><br>
 
-<option value="8">新井研究室</option><br>
+                                <option value="8">新井研究室</option><br>
 
-<option value="9">武田研究室</option><br>
+                                <option value="9">武田研究室</option><br>
 
-<option value="10">下村研究室</option><br>
+                                <option value="10">下村研究室</option><br>
 
-<option value="11">鴻巣研究室</option><br>
+                                <option value="11">鴻巣研究室</option><br>
 
-<option value="12">遠山研究室</option><br>
+                                <option value="12">遠山研究室</option><br>
 
-<option value="13">加藤研究室</option><br>
+                                <option value="13">加藤研究室</option><br>
 
-<option value="14">谷本研究室</option><br>
-   
-<br />
+                                <option value="14">谷本研究室</option><br>
 
-</select>
-               <p><input type="submit" value="登録"></p>
-            </form>
-        </div>
-    </body>
-</html>
+                                <br />
+
+                            </select>
+
+
+
+                            <p><input type="submit" value="登録"></p>
+                            </form>
+                            </div>
+                            </body>
+                            </html>
